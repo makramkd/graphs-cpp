10ed0f419457eb8f4487355c930ac089d5b0fd6c
Makram Kamaleddine <makram.kd@gmail.com>
2015-10-23 18:51:27 -0400

    Adding a class to fill out for graphs.

---
+++ graph.h
@@ -0,0 +1,22 @@
+//
+// Created by Makram Kamaleddine on 10/23/15.
+//
+
+#ifndef GRAPHS_GRAPH_H
+#define GRAPHS_GRAPH_H
+
+/**
+ * A class that represents an undirected graph. There may
+ * be weights on some of the edges.
+ */
+class graph {
+public:
+    graph();
+
+
+private:
+
+};
+
+
+#endif //GRAPHS_GRAPH_H
