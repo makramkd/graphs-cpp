e42172c08a198817bdef40f50a5aa11c5a12a9ec
Makram Kamaleddine <makram.kd@gmail.com>
2015-10-23 19:23:46 -0400

    Adding a directed edge type.

--- edge.h
+++ edge.h
@@ -11,13 +11,24 @@
  * undirected graph. The edge could have a weight: if
  * it doesn't, it's weight would be 0.
  */
-#include <type_traits>
-
 template<typename vertex_type = int,
         typename weight_type = int>
 struct edge {
     vertex_type v1;
     vertex_type v2;
+    weight_type weight;
+};
+
+/**
+ * A class that is supposed to represent an edge in an
+ * undirected graph. The edge could have a weight: if
+ * it doesn't, it's weight would be 0.
+ */
+template<typename vertex_type = int,
+        typename weight_type = int>
+struct d_edge {
+    vertex_type from;
+    vertex_type to;
     weight_type weight;
 };
 
