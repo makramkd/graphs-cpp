9215ce2645ed6290e2805bde5511f29b8f35c39b
Makram Kamaleddine <makram.kd@gmail.com>
2015-10-24 00:49:34 -0400

    Adding some testing code but it seems like the number of edges is incorrect.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,39 @@
 #include <iostream>
-#include "edge.h"
+#include "graph.h"
 
 int main() {
-    edge<std::string, std::string> e;
-    e.v1 = "hello";
-    e.v2 = "world";
-    e.weight = "hahaha";
+    std::cout << "Graph test" << std::endl;
+
+    graph<> g;
+
+    // add some vertices to g
+    for (int i = 0; i < 5; ++i) {
+        g.add_vertex(i);
+    }
+
+    // add some edges
+    g.add_edge(0, 1);
+    g.add_edge(0, 3);
+    g.add_edge(1, 2);
+    g.add_edge(1, 3);
+    g.add_edge(2, 3);
+    g.add_edge(2, 5);
+    g.add_edge(4, 1);
+    g.add_edge(4, 3);
+
+    // print out some adjacency lists
+    for (int i = 0; i < 5; ++i) {
+        std::cout << "Vertex v_" << i << "'s adjacency list:" << std::endl;
+        std::cout << "(";
+        for (auto v : g.adj_list_of(i)) {
+            std::cout << v.vertex << " ";
+        }
+        std::cout << ")" << std::endl;
+    }
+
+    std::cout << "Number of edges in g: " << g.edges() << std::endl;
+    std::cout << "Number of vertices in g: " << g.vertices() << std::endl;
+    std::cout << "Max degree of g: " << g.max_degree() << std::endl;
+    std::cout << "Min degree of g: " << g.min_degree() << std::endl;
+    std::cout << "Average degree of g: " << g.avg_degree() << std::endl;
 }
