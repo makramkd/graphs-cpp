3d37df43ea11e6a7a0d00549ca0ae91d1bf2e587
Makram Kamaleddine <makram.kd@gmail.com>
2015-10-24 01:17:52 -0400

    Looks like edge removal is working. I think I will rewrite vertex removal to just use edge removal.

--- main.cpp
+++ main.cpp
@@ -17,14 +17,14 @@ int main() {
     g.add_edge(1, 2);
     g.add_edge(1, 3);
     g.add_edge(2, 3);
-    g.add_edge(2, 5);
+    g.add_edge(2, 5); // 5 is not in the graph, so this won't add anything
     g.add_edge(4, 1);
     g.add_edge(4, 3);
 
     // print out some adjacency lists
     for (int i = 0; i < 5; ++i) {
         std::cout << "Vertex v_" << i << "'s adjacency list:" << std::endl;
-        std::cout << "(";
+        std::cout << "( ";
         for (auto v : g.adj_list_of(i)) {
             std::cout << v.vertex << " ";
         }
@@ -36,4 +36,14 @@ int main() {
     std::cout << "Max degree of g: " << g.max_degree() << std::endl;
     std::cout << "Min degree of g: " << g.min_degree() << std::endl;
     std::cout << "Average degree of g: " << g.avg_degree() << std::endl;
+
+    // try to remove some edges
+    g.remove_edge(0, 1);
+
+    std::cout << g.edges() << std::endl;
+    std::cout << "( ";
+    for (auto v : g.adj_list_of(0)) {
+        std::cout << v.vertex << " ";
+    }
+    std::cout << ")";
 }
