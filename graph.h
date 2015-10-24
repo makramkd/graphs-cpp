c6323dc3b614a81fdef436435e806ad4a076e00d
Makram Kamaleddine <makram.kd@gmail.com>
2015-10-23 23:00:28 -0400

    Adding primitive constructors to the graph class.

--- graph.h
+++ graph.h
@@ -14,11 +14,29 @@
 template<typename vertex_type = int, typename weight_type = int>
 class graph {
 public:
+    typedef typename adjacency_list<vertex_type, weight_type>::vertex_list vertex_list;
     graph()
+    : adj_list()
     {
 
     }
 
+    graph(int num_vertices)
+    : adj_list(num_vertices)
+    {
+
+    }
+
+    graph(const graph& other)
+    : adj_list(other.adj_list)
+    {
+
+    }
+
+    vertex_list adjList(vertex_type vertex)
+    {
+        return adj_list[vertex];
+    }
 
 private:
     adjacency_list<vertex_type, weight_type> adj_list;
